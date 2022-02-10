c79108bf3c0b1b7d96eaaf0910e1f9366d8a6333
masonrware <86129546+masonrware@users.noreply.github.com>
2022-02-09 19:41:36 -0500

    fixes and updates to null search

--- PA1/course_search.py
+++ PA1/course_search.py
@@ -1,14 +1,14 @@
-'''
+"""
 course_search is a Python script using a terminal based menu to help
 students search for courses they might want to take at Brandeis
-'''
+"""
 
-from schedule import Schedule
+import schedule
 import sys
 
-schedule = Schedule()
+schedule = schedule.Schedule()
 schedule.load_courses()
-schedule = schedule.enrolled(range(5,1000)) # eliminate courses with no students
+schedule = schedule.enrolled(range(5, 1000))  # eliminate courses with no students
 
 TOP_LEVEL_MENU = '''
 quit
@@ -24,46 +24,48 @@ timeofday (filter by day and time, e.g. meets at 11 on Wed)
 
 terms = {c['term'] for c in schedule.courses}
 
+
 def topmenu():
-    '''
+    """
     topmenu is the top level loop of the course search app
-    '''
+    """
     global schedule
-    while True:         
+    while True:
         command = input(">> (h for help) ")
-        if command=='quit':
+        if command == 'quit':
             return
-        elif command in ['h','help']:
+        elif command in ['h', 'help']:
             print(TOP_LEVEL_MENU)
-            print('-'*40+'\n\n')
+            print('-' * 40 + '\n\n')
             continue
-        elif command in ['r','reset']:
+        elif command in ['r', 'reset']:
             schedule.load_courses()
-            schedule = schedule.enrolled(range(5,1000))
+            schedule = schedule.enrolled(range(5, 1000))
             continue
         elif command in ['t', 'term']:
-            term = input("enter a term:"+str(terms)+":")
+            term = input("enter a term:" + str(terms) + ":")
             schedule = schedule.term([term]).sort('subject')
-        elif command in ['s','subject']:
+        elif command in ['s', 'subject']:
             subject = input("enter a subject:")
             schedule = schedule.subject([subject])
         else:
-            print('command',command,'is not supported')
+            print('command', command, 'is not supported')
             continue
 
-        print("courses has",len(schedule.courses),'elements',end="\n\n")
+        print("courses has", len(schedule.courses), 'elements', end="\n\n")
         print('here are the first 10')
         for course in schedule.courses[:10]:
             print_course(course)
-        print('\n'*3)
+        print('\n' * 3)
+
 
 def print_course(course):
-    '''
-    print_course prints a brief description of the course 
-    '''
-    print(course['subject'],course['coursenum'],course['section'],
-          course['name'],course['term'],course['instructor'])
+    """
+    print_course prints a brief description of the course
+    """
+    print(course['subject'], course['coursenum'], course['section'],
+          course['name'], course['term'], course['instructor'])
+
 
 if __name__ == '__main__':
     topmenu()
-
--- PA1/schedule.py
+++ PA1/schedule.py
@@ -5,48 +5,49 @@ by filtering, mapping, printing, etc.
 
 import json
 
-class Schedule():
-    '''
+
+class Schedule:
+    """
     Schedule represent a list of Brandeis classes with operations for filtering
-    '''
-    def __init__(self,courses=()):
+    """
+
+    def __init__(self, courses=()):
         ''' courses is a tuple of the courses being offered '''
         self.courses = courses
 
     def load_courses(self):
-        ''' load_courses reads the course data from the courses.json file'''
+        """ load_courses reads the course data from the courses.json file"""
         print('getting archived regdata from file')
-        with open("courses20-21.json","r",encoding='utf-8') as jsonfile:
+        with open("courses20-21.json", "r", encoding='utf-8') as jsonfile:
             courses = json.load(jsonfile)
         for course in courses:
             course['instructor'] = tuple(course['instructor'])
             course['coinstructors'] = [tuple(f) for f in course['coinstructors']]
         self.courses = tuple(courses)  # making it a tuple means it is immutable
 
-    def lastname(self,names):
+    def lastname(self, names):
         ''' lastname returns the courses by a particular instructor last name'''
         return Schedule([course for course in self.courses if course['instructor'][1] in names])
 
-    def email(self,emails):
+    def email(self, emails):
         ''' email returns the courses by a particular instructor email'''
         return Schedule([course for course in self.courses if course['instructor'][2] in emails])
 
-    def term(self,terms):
+    def term(self, terms):
         ''' email returns the courses in a list of term'''
         return Schedule([course for course in self.courses if course['term'] in terms])
 
-    def enrolled(self,vals):
+    def enrolled(self, vals):
         ''' enrolled filters for enrollment numbers in the list of vals'''
         return Schedule([course for course in self.courses if course['enrolled'] in vals])
 
-    def subject(self,subjects):
+    def subject(self, subjects):
         ''' subject filters the courses by subject '''
         return Schedule([course for course in self.courses if course['subject'] in subjects])
 
-    def sort(self,field):
-        if field=='subject':
-            return Schedule(sorted(self.courses, key= lambda course: course['subject']))
+    def sort(self, field):
+        if field == 'subject':
+            return Schedule(sorted(self.courses, key=lambda course: course['subject']))
         else:
-            print("can't sort by "+str(field)+" yet")
+            print("can't sort by " + str(field) + " yet")
             return self
- 
